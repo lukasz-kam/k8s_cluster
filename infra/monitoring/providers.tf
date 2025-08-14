f869b01111eb57ad92d431f0470d37ca254e709d
lukasz-kam <lukasz.kaminski02@gmail.com>
2025-08-14 18:23:39 +0200

    feat: Add terraform backend to the monitoring infra

--- infra/monitoring/providers.tf
+++ infra/monitoring/providers.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "awsinfra-tfstate-038462790533"
+    key          = "terraform/kube-cluster.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
 provider "helm" {
   kubernetes = {
     config_path = var.config_path
